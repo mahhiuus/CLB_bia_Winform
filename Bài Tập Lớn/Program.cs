3453b77e1da44e6998041b8a6808590ac0ab09d7
nghiadeptrai-dev <nnghia0204@gmail.com>
2026-05-20 17:19:08 +0700

    update login form

--- Bài Tập Lớn/Program.cs
+++ Bài Tập Lớn/Program.cs
@@ -16,7 +16,7 @@ namespace Bài_Tập_Lớn
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new frmLogin());
         }
     }
 }
